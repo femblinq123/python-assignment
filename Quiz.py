4360b091f449db1f2bcf127e1ee2c352a9ad5c1e
Obafemi <obafemiadebayo4gmail.com>
2023-01-20 08:57:30 +0100

    first commit

---
+++ Quiz.py
@@ -0,0 +1,12 @@
+import math
+
+answer_Str = input("enter the name femi age: ")
+answer_int = input(answer_int)
+if answer == "21":
+  print("correct")
+else:
+     print("not correct")
+else:
+     print("try again")
+
+
---
+++ Rain.py
@@ -0,0 +1,9 @@
+import math
+
+inch_Str = input("Enter the inch of your rain: ")
+inch_int = int(inch_Str)
+
+volume_of_rain = float (inch_of_rain_Str * 0.623)
+
+gallons_of_water = volume_of_rain * 231
+print("The gallons of water is :",gallons_of_water)
